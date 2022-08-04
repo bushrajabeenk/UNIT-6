6c0425c4799947375844f78a8653f4af7ace76fb
bushrajabeenk <74173626+bushrajabeenk@users.noreply.github.com>
2022-08-04 11:43:52 +0530

    ch

--- S2/day3/index.js
+++ S2/day3/index.js
@@ -1,10 +1,34 @@
 const express = require("express");
-
+const UserModel = require("./models/user.model");
+const mongoose = require("mongoose");
 const app = express();
+const jwt = require("jsonwebtoken");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.listen(8080, () => {
-  console.log("Server started on port 8080");
+app.post("/signup", async (req, res) => {
+  const { username, password, role } = req.body;
+
+  const user = await new UserModel({
+    username: username,
+    age: 12,
+    hashPassword: password,
+    role: "student", // role from frontend
+  });
+
+  await user.save();
+
+  const token = jwt.sign({
+    id: user._id,
+    username: user.username,
+    role: user.role,
+  });
+  return res.send({ token });
+});
+
+mongoose.connect("mongodb://localhost:27017/web-17").then(() => {
+  app.listen(8080, () => {
+    console.log("Server started on port 8080");
+  });
 });
