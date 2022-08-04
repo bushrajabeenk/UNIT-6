92d6cee45aa1ce9540c1ca374a7f830aa269f519
bushrajabeenk <74173626+bushrajabeenk@users.noreply.github.com>
2022-08-04 11:34:29 +0530

    ch

---
+++ S2/day3/models/user.model.js
@@ -0,0 +1,15 @@
+const { Schema, model } = require("mongoose");
+
+const UserSchema = new Schema({
+  username: String,
+  hashPassword: String,
+  age: Number,
+  role: {
+    type: String,
+    enum: ["student", "instructor", "admin"],
+  },
+});
+
+const UserModel = model("user", UserSchema);
+
+module.exports = UserModel;
