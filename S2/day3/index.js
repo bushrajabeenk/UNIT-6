ea3e1c69d802a0e2ab0a6878da4bdb23341f119f
bushrajabeenk <74173626+bushrajabeenk@users.noreply.github.com>
2022-08-04 12:00:46 +0530

    ch

--- S2/day3/index.js
+++ S2/day3/index.js
@@ -30,6 +30,18 @@ app.post("/signup", async (req, res) => {
   return res.send({ token });
 });
 
+app.delete("/lecture/:lectureid", (req, res) => {
+  const token = req.headers["authorization"].split(" ")[1];
+
+  const { role } = jwt.verify(token, "SECRET");
+
+  if (role !== "admin") {
+    return res.status(403).send("You don't have access to delete a lecture");
+  } else {
+    return res.status(200).send("Lecture deleted successfully");
+  }
+});
+
 mongoose.connect("mongodb://localhost:27017/web-17").then(() => {
   app.listen(8080, () => {
     console.log("Server started on port 8080");
