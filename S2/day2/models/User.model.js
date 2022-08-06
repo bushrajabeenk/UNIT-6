d0146fb94dd50180db423d1c8bedc3973fad5171
bushrajabeenk <74173626+bushrajabeenk@users.noreply.github.com>
2022-08-06 11:42:00 +0530

    add

---
+++ S2/day2/models/User.model.js
@@ -0,0 +1,11 @@
+const { Schema, model } = require("mongoose");
+
+const UserSchema = new Schema({
+  username: String,
+  password: String,
+  age: Number,
+});
+
+const UserModel = model("user", UserSchema);
+
+module.exports = UserModel;
