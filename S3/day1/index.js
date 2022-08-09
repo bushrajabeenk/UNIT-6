27b36ffa0afe46af96f3c6770f51b9627a0cf07d
bushrajabeenk <74173626+bushrajabeenk@users.noreply.github.com>
2022-08-09 11:52:38 +0530

    ch

--- S3/day1/index.js
+++ S3/day1/index.js
@@ -1,17 +1,17 @@
-const { EventEmitter } = require("events");
+// const { EventEmitter } = require("events");
 
-const emmitter = new EventEmitter();
+// const emmitter = new EventEmitter();
 
 // emmitter.on(event, callback)
 // emmitter.emit(event)
 
-emmitter.on("greet", function () {
-  console.log("Someone said hello");
-});
+// emmitter.on("greet", function () {
+//   console.log("Someone said hello");
+// });
 
-emmitter.on("logout", function () {
-  console.log("Someone logged out");
-});
+// emmitter.on("logout", function () {
+//   console.log("Someone logged out");
+// });
 
 // setTimeout(() => {
 //   emmitter.emit("greet");
@@ -21,12 +21,24 @@ emmitter.on("logout", function () {
 //   emmitter.emit("logout");
 // }, 3000);
 
-const stream = createReadStream("test.txt");
+// const stream = createReadStream("test.txt");
 
-stream.on("data", (data) => {
-  console.log(data.toString("utf-8"));
+// stream.on("data", (data) => {
+//   console.log(data.toString("utf-8"));
+// });
+
+// stream.on("end", () => {
+//   console.log("finished file reading");
+// });
+
+const { WebSocketServer } = require("ws");
+
+const wss = new WebSocketServer({ port: 8080 });
+
+wss.on("listening", () => {
+  console.log("Server started, listening for new connections");
 });
 
-stream.on("end", () => {
-  console.log("finished file reading");
+wss.on("connection", (ws) => {
+  console.log("Got new connection");
 });
