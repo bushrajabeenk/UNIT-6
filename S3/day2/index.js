560f2a073c966d6ac77297f58d20a441fa4b2a79
bushrajabeenk <74173626+bushrajabeenk@users.noreply.github.com>
2022-08-10 10:47:23 +0530

    ch

--- S3/day2/index.js
+++ S3/day2/index.js
@@ -1,1 +1,14 @@
 const express = require("express");
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.listen(8080, () => {
+  console.log("Server started on port 8080");
+});
