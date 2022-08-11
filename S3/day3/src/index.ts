3f9a25b5d31574918a973597c7faf5061234fa7e
bushrajabeenk <74173626+bushrajabeenk@users.noreply.github.com>
2022-08-11 12:37:50 +0530

    ch

--- S3/day3/src/index.ts
+++ S3/day3/src/index.ts
@@ -1,9 +1,14 @@
 import express from "express";
 import { UserRouter } from "./routes/user.route";
+import mongoose from "mongoose";
+import config from "config";
+
+//console.log(process.env);
+
 const app = express();
 
 app.use("/users", UserRouter);
 
-app.listen(3000, () => {
+app.listen(config.get("port"), () => {
   console.log("Server started at 3000");
 });
