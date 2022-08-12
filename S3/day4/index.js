f88b4a95b7249bcf85039c0fea5ea415844518e3
bushrajabeenk <74173626+bushrajabeenk@users.noreply.github.com>
2022-08-13 00:01:50 +0530

    ch

--- S3/day4/index.js
+++ S3/day4/index.js
@@ -1,20 +1,24 @@
-const bar = () => console.log("bar settimeout");
+const bar = () => console.log("bar settimeout"); // 7
 
-const baz = () => console.log("baz");
+const baz = () => console.log("baz"); // 3
 
 const foo = () => {
-  setTimeout(bar, 0);
-  console.log("foo");
+  setTimeout(bar, 0); 
+  console.log("foo"); // 2
   new Promise((resolve, reject) => {
-    resolve("should be right after baz, before bar");
+    resolve("should be right after baz, before bar"); // 5
   })
     .then((resolve) => console.log(resolve))
-    .then((resolve) => console.log("8888******"));
+    .then((resolve) => console.log("8888******")); // 6
   baz();
 };
 
-const add = () => console.log("add", 2 + 3);
+const add = () => console.log("add", 2 + 3); // 1
 
 add();
 foo();
-console.log("foood");
+console.log("foood"); // 4
+
+
+// Queue: bar
+// Promise queue: 8888******, should be right after baz, before bar
