9d50003c62ffde5dc9c86e4e5edb5ba7547016e3
bushrajabeenk <74173626+bushrajabeenk@users.noreply.github.com>
2022-08-13 23:09:06 +0530

    ch

--- S3/day1/c.js
+++ S3/day1/c.js
@@ -2,21 +2,28 @@
 // socket.io is another websocket library
 const { WebSocketServer } = require("ws");
 
+// server is created
 const wss = new WebSocketServer({ port: 8080 });
 
 // wss.on then ctrl+space will give the list of all the
 // events present on wss..
 // listening happens when server is in listening mode
 // events listening/close/connection/error/headers are predefined events available on wss
+// server is started
+// ready to accept new connections
 wss.on("listening", () => {
   console.log("Server started, listening for new connections");
 });
 
-// when client establishes a connection with server
+// when client/user/frontend establishes a connection with server
 wss.on("connection", (ws) => {
   console.log("Got new connection");
-  ws.on("message", (data) => {
-    console.log("Socket sent message:", data.toString("utf-8"));
-    ws.send("Hi back");
-  });
 });
+
+// wss.on("connection", (ws) => {
+//   console.log("Got new connection");
+//   ws.on("message", (data) => {
+//     console.log("Socket sent message:", data.toString("utf-8"));
+//     ws.send("Hi back");
+//   });
+// });
