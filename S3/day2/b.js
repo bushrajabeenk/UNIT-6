5b71dbcd189bd6fb9c825ee4a0d00a2b63c28df9
bushrajabeenk <74173626+bushrajabeenk@users.noreply.github.com>
2022-08-14 00:33:18 +0530

    ch

--- S3/day2/b.js
+++ S3/day2/b.js
@@ -7,27 +7,9 @@ const webServer = require("http").createServer(app);
 
 const wss = new Server(webServer);
 
-const history = [];
-
 wss.on("connection", (ws) => {
   console.log("New user joined");
-
-  // every single socket except current socket
-  ws.broadcast.emit("new User");
-
-  ws.emit("history", history);
-
-  ws.on("new message", (msgg) => {
-    console.log("Got new message", msgg);
-
-    history.push(msgg);
-
-    // use ws to get the message on all the users except the sender user
-    // ws.emit("new message", msgg);
-
-    // use wss to get the message on all the users including itself
-    wss.emit("new message", msgg);
-  });
+  ws.send("hello user");
 });
 
 app.use(express.urlencoded({ extended: true }));
