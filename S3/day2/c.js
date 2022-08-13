cf022ad396f79610490c80c60c1d29052ff359ea
bushrajabeenk <74173626+bushrajabeenk@users.noreply.github.com>
2022-08-14 01:16:10 +0530

    ch

--- S3/day2/c.js
+++ S3/day2/c.js
@@ -1,25 +1,28 @@
 const express = require("express");
 const { Server } = require("socket.io");
 
-const app = express(); // express only for features
+//created the routing with express
+const app = express(); // express only for features, i.e routes and functionalities only
 
-const webServer = require("http").createServer(app);
+const httpServer = require("http").createServer(app);
 
-const wss = new Server(webServer);
+const wss = new Server(httpServer);
 
 wss.on("connection", (ws) => {
   console.log("New user joined");
   ws.send("hello user");
 });
 
+// app has middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// app has routes
 // sendFile is express operation, not OS- file/fs operation
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/c.html");
 });
 
-webServer.listen(8080, () => {
+httpServer.listen(8080, () => {
   console.log("Server started on port 8080");
 });
