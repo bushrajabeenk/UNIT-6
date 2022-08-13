22b5aaeb825c10d305c7dc3022da7c15d3fca713
bushrajabeenk <74173626+bushrajabeenk@users.noreply.github.com>
2022-08-14 00:11:11 +0530

    ch

--- S3/day2/a.js
+++ S3/day2/a.js
@@ -1,25 +1,5 @@
-const express = require("express");
-const { Server } = require("socket.io");
+//creating http server in Node.js without using express
 
-const app = express(); // express only for features
+const http = require("http");
 
-const webServer = require("http").createServer(app);
-
-const wss = new Server(webServer);
-
-wss.on("connection", (ws) => {
-  console.log("New user joined");
-  ws.send("hello user");
-});
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-// sendFile is express operation, not OS- file/fs operation
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/public/index.html");
-});
-
-webServer.listen(8080, () => {
-  console.log("Server started on port 8080");
-});
+const server = http.createServer();
--- S3/day2/c.js
+++ S3/day2/c.js
@@ -1,4 +1,25 @@
-const http = require("http");
+const express = require("express");
+const { Server } = require("socket.io");
 
-//creating server without using express
-const server = http.createServer();
+const app = express(); // express only for features
+
+const webServer = require("http").createServer(app);
+
+const wss = new Server(webServer);
+
+wss.on("connection", (ws) => {
+  console.log("New user joined");
+  ws.send("hello user");
+});
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// sendFile is express operation, not OS- file/fs operation
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+webServer.listen(8080, () => {
+  console.log("Server started on port 8080");
+});
