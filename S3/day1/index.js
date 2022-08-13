38573bb5fc40635ba720b21115864f89b8562c5a
bushrajabeenk <74173626+bushrajabeenk@users.noreply.github.com>
2022-08-13 21:01:36 +0530

    ch

--- S3/day1/index.js
+++ S3/day1/index.js
@@ -1,15 +1,27 @@
 // 1. server needs to send data without client making requests
-// 2. data constantly chnaging stream 
+// 2. data constantly chnaging stream
 // Node.js internal package called emitter
+
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------
+
+// const event = require("events");
+// const emitter = new event.EventEmitter();
+// emitter.on("greet", function () {
+//   console.log("Someone said hello");
+// });
+// emitter.emit("greet");
+
+// OR directly as EventEmitter from events
 
 // const { EventEmitter } = require("events");
 // events is a nodejs module
 
 // const emmitter = new EventEmitter();
-
-// emmitter.on(event, callback)
+// emmitter.on(eventName, callback)
 // emmitter.emit(event)
 
+// when eventName is greet, print Someone said hello
 // emmitter.on("greet", function () {
 //   console.log("Someone said hello");
 // });
@@ -26,6 +38,9 @@
 //   emmitter.emit("logout");
 // }, 3000);
 
+//----------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------------------------
+
 // const stream = createReadStream("test.txt");
 
 // stream.on("data", (data) => {
@@ -36,19 +51,19 @@
 //   console.log("finished file reading");
 // });
 
-const { WebSocketServer } = require("ws");
+// const { WebSocketServer } = require("ws");
 
-const wss = new WebSocketServer({ port: 8080 });
+// const wss = new WebSocketServer({ port: 8080 });
 
-wss.on("listening", () => {
-  console.log("Server started, listening for new connections");
-});
+// wss.on("listening", () => {
+//   console.log("Server started, listening for new connections");
+// });
 
 // when client establishes a connection with server
-wss.on("connection", (ws) => {
-  console.log("Got new connection");
-  ws.on("message", (data) => {
-    console.log("Socket sent message:", data.toString("utf-8"));
-    ws.send("Hi back");
-  });
-});
+// wss.on("connection", (ws) => {
+//   console.log("Got new connection");
+//   ws.on("message", (data) => {
+//     console.log("Socket sent message:", data.toString("utf-8"));
+//     ws.send("Hi back");
+//   });
+// });
