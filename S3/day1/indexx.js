fefbf5b0c5fc099dc1a6f0558c2bd9dee6f45712
bushrajabeenk <74173626+bushrajabeenk@users.noreply.github.com>
2022-08-13 21:14:40 +0530

    ch

--- S3/day1/indexx.js
+++ S3/day1/indexx.js
@@ -1,3 +1,4 @@
+//reading a file through stream
 const stream = createReadStream("test.txt");
 
 stream.on("data", (data) => {
@@ -6,21 +7,4 @@ stream.on("data", (data) => {
 
 stream.on("end", () => {
   console.log("finished file reading");
-});
-
-const { WebSocketServer } = require("ws");
-
-const wss = new WebSocketServer({ port: 8080 });
-
-wss.on("listening", () => {
-  console.log("Server started, listening for new connections");
-});
-
-// when client establishes a connection with server
-wss.on("connection", (ws) => {
-  console.log("Got new connection");
-  ws.on("message", (data) => {
-    console.log("Socket sent message:", data.toString("utf-8"));
-    ws.send("Hi back");
-  });
 });
---
+++ S3/day1/indexxx.js
@@ -0,0 +1,16 @@
+const { WebSocketServer } = require("ws");
+
+const wss = new WebSocketServer({ port: 8080 });
+
+wss.on("listening", () => {
+  console.log("Server started, listening for new connections");
+});
+
+// when client establishes a connection with server
+wss.on("connection", (ws) => {
+  console.log("Got new connection");
+  ws.on("message", (data) => {
+    console.log("Socket sent message:", data.toString("utf-8"));
+    ws.send("Hi back");
+  });
+});
