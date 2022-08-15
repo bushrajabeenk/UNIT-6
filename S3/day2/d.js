567542823a59227aa643da4d61a2eaada13b3e73
bushrajabeenk <74173626+bushrajabeenk@users.noreply.github.com>
2022-08-15 13:25:33 +0530

    ch

--- S3/day2/d.js
+++ S3/day2/d.js
@@ -7,14 +7,24 @@ const webServer = require("http").createServer(app);
 
 const wss = new Server(webServer);
 
+const history = [];
+
 wss.on("connection", (ws) => {
-  ws.emit("new user");
+  ws.broadcast.emit("new user", history);
+  ws.emit("history", history);
 
+  // ws.on("new message", (m) => {
+  //   // use ws to get the message on all the users except the sender user
+  //   // ws.broadcast.emit("new message", m);
+
+  //   // use wss to get the message on all the users including itself
+  //   wss.emit("new message", m);
+  // });
+});
+
+wss.on("connection", (ws) => {
   ws.on("new message", (m) => {
-    // use ws to get the message on all the users except the sender user
-    // ws.broadcast.emit("new message", m);
-
-    // use wss to get the message on all the users including itself
+    history.push(m);
     wss.emit("new message", m);
   });
 });
@@ -24,7 +34,7 @@ app.use(express.json());
 
 // sendFile is express operation, not OS- file/fs operation
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/public/d.html");
+  res.sendFile(__dirname + "/d.html");
 });
 
 webServer.listen(8080, () => {
