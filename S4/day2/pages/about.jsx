3bb80b541eedc9c93505268b0f1def20439fbc99
bushrajabeenk <74173626+bushrajabeenk@users.noreply.github.com>
2022-08-18 19:25:18 +0530

    ch

--- S4/day2/pages/about.jsx
+++ S4/day2/pages/about.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import Navbar from "../components/Navbar";
 
 const about = () => {
   return (
     <div>
+      <Navbar />
       <h2>About</h2>
       <p>We are a good company</p>
     </div>
--- S4/day2/pages/welcome.jsx
+++ S4/day2/pages/welcome.jsx
@@ -1,9 +1,12 @@
 import React, { useState } from "react";
+import Navbar from "../components/Navbar";
 
 const welcome = () => {
   const [count, setCount] = useState(0);
   return (
     <div>
+      <Navbar />
+
       <h3>Counter: {count}</h3>
       <div>
         <button onClick={() => setCount(count + 1)}>Click me</button>
