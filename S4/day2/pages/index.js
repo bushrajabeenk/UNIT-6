71213397a41254a6a18d83253c7a97020b2eea96
bushrajabeenk <74173626+bushrajabeenk@users.noreply.github.com>
2022-08-18 11:20:39 +0530

    ch

--- S4/day2/pages/index.js
+++ S4/day2/pages/index.js
@@ -1,3 +1,10 @@
 export default function Home() {
-  return <div>Hello world</div>;
+  return (
+    <div>
+      <h3>Counter: 3</h3>
+      <div>
+        <button>Click me</button>
+      </div>
+    </div>
+  );
 }
