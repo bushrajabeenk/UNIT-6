7445b177c13a5e36b3af0462a6a003dd3a8f3e96
bushrajabeenk <74173626+bushrajabeenk@users.noreply.github.com>
2022-08-19 01:36:58 +0530

    ch

--- S4/day2/pages/products/[productid].jsx
+++ S4/day2/pages/products/[productid].jsx
@@ -1,26 +1,54 @@
+// import { useRouter } from "next/router";
+// import { useEffect, useState } from "react";
+
+// const url = "https://jsonplaceholder.typicode.com/users";
+
+// export default function ProductDetails() {
+//   const [data, setData] = useState({});
+//   const { productid } = useRouter().query;
+
+//   useEffect(() => {
+//     fetch(`${url}/${productid}`)
+//       .then((x) => x.json())
+//       .then((data) => {
+//         console.log(data);
+//         setData(data);
+//       });
+//   }, [productid]);
+
+//   return (
+//     <div>
+//       User details of id: {productid}
+//       <div>Name: {data.name}</div>
+//       <div>Email: {data.email}</div>
+//     </div>
+//   );
+// }
+
 import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
 
 const url = "https://jsonplaceholder.typicode.com/users";
 
-export default function ProductDetails() {
-  const [data, setData] = useState({});
+export default function ProductDetails({ userDetails }) {
   const { productid } = useRouter().query;
-
-  useEffect(() => {
-    fetch(`${url}/${productid}`)
-      .then((x) => x.json())
-      .then((data) => {
-        console.log(data);
-        setData(data);
-      });
-  }, [productid]);
 
   return (
     <div>
       User details of id: {productid}
-      <div>Name: {data.name}</div>
-      <div>Email: {data.email}</div>
+      <div>Name: {userDetails.name}</div>
+      <div>Email: {userDetails.email}</div>
     </div>
   );
 }
+
+export const getServerSideProps = async (context) => {
+  const { productid } = context.params;
+
+  const data = await fetch(`${url}/${productid}`).then((x) => x.json());
+
+  return {
+    props: {
+      userDetails: data,
+    },
+  };
+};
